dc2126b6f03e83767d762c2cbc80a6cd39a04818
Prasanth_92 <prasanth.rs@oceansoftwares.in>
2022-09-05 11:45:06 +0530

    first commit

---
+++ backend/db/mongo.js
@@ -0,0 +1,14 @@
+// const mongoose = require("mongoose");
+
+// mongoose.createConnection('mongodb://localhost/ibafss',{useNewUrlParser: true, useUnifiedTopology: true},err => {
+//   if(!err)
+//       {
+//           console.log("DB Connected");
+//       }
+//       else{
+//         { console.log('Error in DB connection : ' + err) }
+//       }
+// });
+
+// module.exports = mongoose;
+
